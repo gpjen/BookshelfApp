fd329848f4da23a210950269b4edfc1cd269f4a3
agan95 <nyonggpj9995@gmail.com>
2022-07-26 18:21:23 +0900

    fix html css interface

---
+++ script.js
@@ -0,0 +1,32 @@
+// data
+const books = [
+    {
+      id: 3657848524,
+      title: "Harry Potter and the Philosopher's Stone",
+      author: "J.K Rowling",
+      year: 1997,
+      isComplete: false,
+    },
+    {
+      id: 128637612,
+      title: "Minah sang mata-mata NATO",
+      author: "Kiliwuk",
+      year: 2022,
+      isComplete: false,
+    },
+    {
+      id: 33356753,
+      title: "Ajudan jatuh cinta",
+      author: "mihawk",
+      year: 2020,
+      isComplete: true,
+    },
+    {
+      id: 128637612,
+      title: "melirik kardus kosong",
+      author: "anadika",
+      year: 2021,
+      isComplete: false,
+    },
+  ];
+
