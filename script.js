4de87139ba68ff77ed117fc80f93e3aba9854401
gpjen95 <nyonggpj9995@gmail.com>
2022-10-02 15:05:02 -0700

    built popup

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("popup-swap").addEventListener("click", (e) => {
+  document.getElementById("popup-swap").classList.remove("swipe-show")
+  document.getElementById("popup-swap").classList.add("swipe-hide")
+})
